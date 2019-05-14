416afcaf9d84913d2b87f5dd764515f6558578e8
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-14 20:47:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,9 @@
 # Add  code here!
 def prime?(n)  #given a variable n
-  prime = false
-  for i in 2..n-1
+  if n < 0
+    prime = false
+  else
+    for i in 2..n-1
     if  n % i == 0
       prime = true
     end
