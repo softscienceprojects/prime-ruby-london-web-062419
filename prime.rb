16d66ad3dc6af2c22bb7766fec841f213bd00b0f
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-14 19:47:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(n)  #given a variable n
+  for i in 1..n-1
+    puts i
+
+    # i / n !== 1 || 0
+  end
+end
+
+# WILSON'S THEOREM
+# n > 1
+# product of all integers less than n
+# is one less
+# than a mulitple of n
