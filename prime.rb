046fca204cfb2616c24e889567ff3ccd669e413f
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-14 20:37:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(n)  #given a variable n
   prime = true
   for i in 2..n-1
-    if  n % i == 0 || n < 0
+    if  n % i == 0
       prime = false
     end
     # i / n !== 1 || 0
@@ -15,3 +15,9 @@ end
 # product of all integers less than n
 # is one less
 # than a mulitple of n
+
+def neg(n)
+  for i in 2..n-1
+    puts "n: #{n} i: #{i}"
+  end
+end
